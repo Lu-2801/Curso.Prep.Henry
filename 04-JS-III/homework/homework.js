4a6e16b121029d608af9fbd331d90b07a6483edf
Luciana <castrolu387@gmail.com>
2021-12-06 19:39:11 -0300

    homework 3 finished

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -273,7 +273,8 @@ function continueStatement(numero) {
   var array = [];
   var suma = numero;
   for(var i= 0; i<10; i++) {
-    if(i === 5) continue;
+    if(i === 5) { 
+      continue; }
     else {
       suma = suma + 2;
       array.push(suma);
